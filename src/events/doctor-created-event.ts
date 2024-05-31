5d4c2c215b0c21f146cdb37dcb1061137f6d117b
LuClaclaws <138918725+LuClaclaws@users.noreply.github.com>
2024-05-31 11:03:10 -0400

    Updates

--- src/events/doctor-created-event.ts
+++ src/events/doctor-created-event.ts
@@ -3,6 +3,7 @@ import { Subjects } from "./subjects";
 export interface DoctorCreatedEvent {
     subject: Subjects.DoctorCreated;
     data: {
+        doctorId: string,
         userId: string
     }
 }
--- src/events/patient-created-event.ts
+++ src/events/patient-created-event.ts
@@ -3,6 +3,7 @@ import { Subjects } from "./subjects";
 export interface PatientCreatedEvent {
     subject: Subjects.PatientCreated;
     data: {
+        patientId: string,
         userId: string
     }
 }
